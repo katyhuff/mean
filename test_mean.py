b48eadbe8715011563e3db739c87824841269dd4
Nishant Maniam <nishant.maniam@gmail.com>
2017-06-07 19:54:39 +0000

    Ready to Test

---
+++ test_mean.py
@@ -0,0 +1,44 @@
+from mean import *
+
+def test_ints():
+    num_list = [1,2,3,4,5]
+    obs = mean(num_list)
+    exp = 3
+    assert obs == exp
+
+def test_zero():
+    num_list=[0,2,4,6]
+    obs = mean(num_list)
+    exp = 3
+    assert obs == exp
+
+def test_double():
+    # This one will fail in Python 2
+    num_list=[1.0000001,2,3,4, -.0000001]
+    obs = mean(num_list)
+    print(sum(num_list))
+    exp = 2.0
+    print(obs)
+    assert obs == exp
+
+def test_long():
+    big = 100000000
+    obs = mean(range(1,big))
+    exp = big/2.0
+    assert obs == exp
+
+def test_complex():
+    # given that complex numbers are an unordered field
+    # the arithmetic mean of complex numbers is meaningless
+    num_list = [2 + 3j, 3 + 4j, -32 - 2j]
+    obs = mean(num_list)
+    exp = NotImplemented
+    assert obs == exp
+
+def test_all():
+    test_ints()
+    test_zero()
+    test_double()
+    test_long()
+    test_complex()
+    
